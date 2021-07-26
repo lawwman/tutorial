743af81c79520311a2f45e6df8617f7d01e8a80b
lawwman <lawrenciok@gmail.com>
2021-07-26 15:46:27 +0800

    update learn_groupby

--- learn_python/learn_pandas/learn_groupby.py
+++ learn_python/learn_pandas/learn_groupby.py
@@ -2,7 +2,20 @@ import pandas as pd
 import numpy as np
 # https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html
 
-simple_df = pd.DataFrame({'Animal': ['Falcon', 'Falcon', 'Parrot', 'Parrot'], 'Max Speed': [380., 370., 24., 26.]})
+simple_df = pd.DataFrame(
+    {
+        'Animal': ['Falcon', 'Falcon', 'Parrot', 'Parrot'], 
+        'Max Speed': [380., 370., 24., 26.]
+    }
+)
+
+simple_df2 = pd.DataFrame(
+    {
+        'Movement': ['Fly', 'Fly', 'Fly', 'Fly', 'Gallop', 'Gallop'],
+        'Animal': ['Falcon', 'Falcon', 'Parrot', 'Parrot', 'Horse', 'Donkey'], 
+        'Max Speed': [380., 370., 24., 26., 30., 15.]
+    }
+)
 
 freq_df = pd.DataFrame(
    {
@@ -34,13 +47,30 @@ freq_df2 = pd.DataFrame(
 def simple_groupby_example():
     df = simple_df.copy(deep=True)
     df1 = df.groupby('Animal').mean()
-    print(df1)  # 'Animal' column is now the new index. Can verify with df1.index
+    print(df1)
     #         Max Speed
     # Animal           
     # Falcon      375.0
     # Parrot       25.0
+
+    # NOTE: 'Animal' column is now the new index. Can verify with print(df1.index)
+    # We can disable this with `as_index=False`.
     df2 = df.groupby('Animal', as_index=False).mean()  # 'Animal' column is not index. their iloc position is index.
     print(df2)
+
+
+def simple_groupby_example2():
+    df = simple_df2.copy(deep=True)
+    df1 = df.groupby(['Animal', 'Movement']).mean()
+    print(df1)
+    print(df1.index)
+    #                     Max Speed
+    # Animal Movement           
+    # Donkey Gallop         15.0
+    # Falcon Fly           375.0
+    # Horse  Gallop         30.0
+    # Parrot Fly            25.0
+    # NOTE: 'Animal' and 'Movement' are now columns.
 
 
 def groupby_obj_attributes():
@@ -48,7 +78,7 @@ def groupby_obj_attributes():
     groupby_obj = df.groupby(pd.Grouper(key='Animal'))  # returns a groupby object
 
     print(groupby_obj.groups)  # {'Falcon': [0, 1], 'Parrot': [2, 3]}
-    print(len(groupby_obj))  # return length of .groups dict. ans: 2
+    print(len(groupby_obj))  # return length of `.groups` dict. ans: 2
 
 
 def simple_grouper_example():
@@ -61,7 +91,7 @@ def simple_grouper_example():
 def simple_grouper_freq_example():
     df = freq_df.copy(deep=True)
     df1 = df.groupby(pd.Grouper(key="Publish date", freq="5 min")).max()
-#     print(df1)  # notice that NaN objects are added to fill in the gap, ensuring 5 min windows.
+    # print(df1)
     #                                 ID  Price
     # Publish date                         
     # 2000-01-01 00:00:00+08:00  0.0   10.0
@@ -71,10 +101,26 @@ def simple_grouper_freq_example():
     # 2000-01-01 00:20:00+08:00  NaN    NaN
     # 2000-01-01 00:25:00+08:00  3.0   40.0
 
+    # NOTE: Behavior I observe: NaN objects are added to fill in the gap, ensuring 5 min windows.
+    # This Behavior is only present when the first index is 'Publish date'. 
+    # If we group by "ID" column first, the gaps are allowed.
+    # groupby(["ID", pd.Grouper(key="Publish date", freq="5 min")]).
+
     df = freq_df2.copy(deep=True)
     df2 = df.groupby(["ID", pd.Grouper(key="Publish date", freq="5 min")]).max()
-#     print(df2)
-    print(df2["Publish date"])
+    print(df2)
+    #                                             Publish date
+    # ID Publish date                                       
+    # 0  2000-01-01 00:00:00+08:00   2000-01-01 00:00:00+08:00
+    #    2000-01-01 00:05:00+08:00   2000-01-01 00:07:00+08:00
+    #    2000-01-01 00:25:00+08:00   2000-01-01 00:26:00+08:00
+    # 1  2000-01-01 01:00:00+08:00   2000-01-01 01:00:00+08:00
+    #    2000-01-01 01:05:00+08:00   2000-01-01 01:07:00+08:00
+    #    2000-01-01 01:25:00+08:00   2000-01-01 01:26:00+08:00
+
+    # NOTE: 'Publish date' is used again for col values. df2 only has 'ID' and 'Publish date' columns.
+    # Because both are used as MultiIndex, 'Publish date' is used again. If df2 has another column, that
+    # column will be used instead.
 
 
-groupby_obj_attributes()
+simple_grouper_freq_example()
